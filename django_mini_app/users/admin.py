a474942df2b8367a51548c49c13d4b329bedc87e
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-05-22 11:47:16 +0400

    !

--- django_mini_app/users/admin.py
+++ django_mini_app/users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+admin.site.register(User)       # показать модель User в админке.
+
+# запускаем сервер
+# py manage.py runserver 9000
+# открываем админку http://127.0.0.1:9000/admin
+
+"""
+Админка позволяет:
+- добавлять пользователей
+- редактировать
+- удалять
+- смотреть данные БД
+
+"""
+
--- django_mini_app/users/models.py
+++ django_mini_app/users/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+# Миграции — это система изменения базы данных в Django.
+class User(models.Model):                       # Наследование от Django модели.
+    # поля
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name                        # объект отображается в admin panel.
+    
+
+"""
+Далее создаем миграции
+py manage.py makemigrations
+
+Django создаст файл:
+users/migrations/0001_initial.py
+
+Применяем миграции
+py manage.py migrate
+Теперь таблица реально создается в базе данных SQLite.
+"""

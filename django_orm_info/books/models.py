2593165860bbe560caab2e3ca07cd01189d48906
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-05-25 11:18:29 +0400

    !

--- django_orm_info/books/models.py
+++ django_orm_info/books/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+class Author(models.Model):
+    name = models.CharField(max_length=100)
 
-# Create your models here.
+    def __str__(self):
+        return self.name
+    
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+    
+class Book(models.Model):
+
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return f"{self.title, self.author, self.category}"
